c87d22c9fdfd62d65d9bf634bf44fd59bcff1c89
yuekcc <yuekcc@qq.com>
2022-09-12 22:40:46 +0800

    更新 manifest 设置

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,13 @@ import vuePlugin from '@vitejs/plugin-vue';
 export default defineConfig({
   base: '/public/',
   plugins: [vuePlugin()],
+  build: {
+    manifest: false,
+  },
   server: {
     proxy: {
       '/api': {
-        target: `http://127.0.0.1:3000`,
+        target: `http://127.0.0.1:10088`,
       },
     },
   },
