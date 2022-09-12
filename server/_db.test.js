da1314234c31f81e75282fc391fd6554b407b184
yuekcc <yuekcc@qq.com>
2022-09-12 18:32:50 +0800

    删除无用代码

--- server/_db.test.js
+++
@@ -1,5 +0,0 @@
-import { findWords } from './_db.js';
-
-findWords(['汉', '字']).forEach(it => {
-  console.log(it.entry, it.paraphrase);
-});
--- server/_word.js
+++
@@ -1,7 +0,0 @@
-import jieba from '@node-rs/jieba';
-
-jieba.load();
-
-export function cutWords(str) {
-  return jieba.cutAll(str);
-}
